dcc65ad05f58d2b94afeae05ec2772bbbb2e93c8
codeclub <codeclub@smms-rm23-35.smusd.local>
2017-12-06 14:58:24 -0800

    Spiral

--- src/day2/MyFirstJavaProgram.java
+++ src/day2/MyFirstJavaProgram.java
@@ -8,8 +8,21 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+	Robot Spudnick = new Robot();
+	Spudnick.turn(360/7);
+	Spudnick.setSpeed(10);
+	Spudnick.penDown();
+	for(int x = 0; x < 75; x++) {
+	Spudnick.move(x*5);
+	Spudnick.setRandomPenColor();
+	Spudnick.setPenWidth(x);
+	}
 	
-		
+	
+	//Spudnick.penDown();
+	//Spudnick.move(250);
+	//Spudnick.turn(90);
+	
 		
 	}
 }
--- src/day2/Spiral.java
+++ src/day2/Spiral.java
@@ -6,20 +6,22 @@ public class Spiral {
 
 	public static void main(String[] args) {
 		// 1. Create a new Robot
-		
+		Robot Spudnick = new Robot();
 		// 5. Set your robot's pen to the down position
-		
+		Spudnick.penDown();
 		// 3. Set the robot to go at max speed (10)
-		
+		Spudnick.setSpeed(10);
 		// 4. Do the following (steps 6-8) 75 times
-		
+		for(int x = 0; x < 75; x++) {
 			// 7. Change the pen color to random
-		
+			Spudnick.setRandomPenColor();
 			// 6. Move the robot 5 times the current line number you are drawing (5*i)
-			
+			Spudnick.move(x*5);
 			// 2. Turn the robot 1/7 of 360 degrees to the right
-		
+		Spudnick.turn(360/7);
 			// 8. Set the pen width to i
-			
+		Spudnick.setPenWidth(x);
+	}
 	}
 }
+
